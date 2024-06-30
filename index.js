59fe788c3d5799b472a281e70cfaec31aa6ef231
David Waruhiu Njenga <davenjenga098@gmail.com>
2024-06-30 13:20:27 +0300

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+// let globalvar = "i'm Global Variables"
+
+// function foot(){
+//     console.log(globalvar)
+// }
+// foot()
+
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+upperCaseCustomerName(toUpperCase())
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+ 
+}
+setBestCustomer();
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+    const leastFavoriteCustomer = 'initial value'
+
+function changeLeastFavoriteCustomer(){
+   let leastFavoriteCustomer = 'new value'
+}
+changeLeastFavoriteCustomer();
+
